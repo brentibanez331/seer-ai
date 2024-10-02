7bdccdd9326f1d7a1a23c5b738b3e8dae4ae44b2
brentibanez331 <94149036+brentibanez331@users.noreply.github.com>
2024-10-03 03:47:29 +0800

    feat: added clear chat functionality

--- src/components/ChatModal.tsx
+++ src/components/ChatModal.tsx
@@ -13,6 +13,13 @@ import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { IoSend } from "react-icons/io5";
+import { FaEraser } from "react-icons/fa6";
+import React from 'react'
+import ReactDom from 'react-dom'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
+
 
 interface ChatModalProps {
     // Define any props needed
@@ -41,7 +48,7 @@ const ChatModal: React.FC<ChatModalProps> = (props) => {
     }, [isModalOpen]);
 
     const promptModel = async (prompt: string) => {
-        if(!props.modelId) {
+        if (!props.modelId) {
             console.error("Model ID is missing")
             return
         }
@@ -90,18 +97,24 @@ const ChatModal: React.FC<ChatModalProps> = (props) => {
 
         const modelMessage = await promptModel(temp)
 
-        if(modelMessage !== ""){
+        if (modelMessage) {
+            
+
             setMessages((prevMessages) => [
                 { role: "model", message: modelMessage },
                 ...prevMessages,
             ]);
         }
-        
+
     };
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setNewMessage(event.target.value);
     };
+
+    const clearMessages = () => {
+        setMessages([])
+    }
 
     return (
         <div className="buttonChat">
@@ -109,29 +122,38 @@ const ChatModal: React.FC<ChatModalProps> = (props) => {
                 <DialogTrigger asChild>
                     <IoChatboxEllipsesOutline />
                 </DialogTrigger>
-                <DialogContent className="sm:max-w-[800px] sm:h-4/5 p-0 flex flex-col rounded-lg overflow-hidden gap-0">
-                    <DialogHeader className="p-4 sticky">
-                        <DialogTitle>Chat with your LLM</DialogTitle>
-                        <DialogDescription>
-                            Test your model here
-                        </DialogDescription>
+                <DialogContent className="sm:max-w-[800px] sm:h-4/5 p-0 flex flex-col rounded-lg overflow-hidden gap-0 ">
+                    <DialogHeader className="p-4 sticky border-b border-neutral-600">
+                        <div className="flex justify-between pr-16">
+                            <div className="space-y-3">
+                                <DialogTitle>Chat with your LLM</DialogTitle>
+                                <DialogDescription>
+                                    Test your model here
+                                </DialogDescription>
+                            </div>
+                            <Button className="text-white bg-red-400 hover:bg-red-300 space-x-2 transition ease-in" onClick={clearMessages}>
+                                <FaEraser/>
+                                <p>CLEAR</p>
+                            </Button>
+                        </div>
                     </DialogHeader>
                     {/* <div className="bg-white p-4 h-min sticky top-0 z-10">
                         <DialogTitle>Chat with your LLM</DialogTitle>
                     </div> */}
-                    <div className="bg-neutral-100 flex-1 flex-col-reverse overflow-y-auto text-sm">
+                    <div className="bg-neutral-800 flex-1 flex-col-reverse overflow-y-auto text-sm">
                         <div className="flex flex-col-reverse p-3">
                             <div ref={messagesEndRef} />
                             {messages.map((msg, index) => (
                                 <div key={index} className={`flex ${msg.role === 'user' ? 'justify-end' : 'justify-start'} mb-2`}>
-                                    <div className={`p-3 rounded-lg ${msg.role === 'user' ? 'ml-auto bg-blue-400 text-white' : 'mr-auto bg-neutral-200'} w-fit max-w-[75%]`}>
+                                    <div className={`p-3 rounded-lg ${msg.role === 'user' ? 'ml-auto bg-blue-400 text-white' : 'mr-auto bg-neutral-700'} w-fit max-w-[75%]`}>
                                         <p>{msg.message}</p>
+                                        {/* <ReactMarkdown remarkPlugins={[remarkGfm]} children={msg.message}></ReactMarkdown> */}
                                     </div>
                                 </div>
                             ))}
                         </div>
                     </div>
-                    <div className="bg-neutral-100 p-3 sticky bottom-0 flex items-center space-x-5 z-10">
+                    <div className="bg-neutral-800 p-3 sticky bottom-0 flex items-center space-x-5 z-10">
                         <Input className="rounded-lg" value={newMessage} onChange={handleInputChange} onKeyPress={(e) => e.key === 'Enter' && handleOnSendMessage()} />
                         <IoSend className="size-7 hover:opacity-80 transition ease-in-out cursor-pointer" onClick={handleOnSendMessage} />
                     </div>
