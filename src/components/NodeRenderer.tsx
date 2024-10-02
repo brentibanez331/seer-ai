1996aa0be20e605d290db55bbb9ede8e9281184e
brentibanez331 <94149036+brentibanez331@users.noreply.github.com>
2024-10-03 03:46:28 +0800

    feat: render new nodes

--- src/components/NodeRenderer.tsx
+++ src/components/NodeRenderer.tsx
@@ -1,10 +1,14 @@
 import { ChatModelNodeComponent } from "./ChatModelComponent";
 import { DocumentLoaderNodeComponent } from "./DocumentLoaderComponent";
+import { EmbeddingNodeComponent } from "./EmbeddingComponent";
 import { BaseNode } from "./interfaces/BaseNode";
 import { ChatModelNodeProps, NodeProps } from "./interfaces/NodeProps";
 import { OutputNodeComponent } from "./OutputNodeComponent";
+import { RetrievalQAComponent } from "./RetrievalQAComponent";
+import { RetrieverNodeComponent } from "./RetrieverComponent";
 import { SystemInstructionNodeComponent } from "./SystemInstructionComponent";
 import { TextSplitterNodeComponent } from "./TextSplitterComponent";
+import { VectorStoreNodeComponent } from "./VectorStoreComponent";
 
 const NodeRenderer = (props: any) => {
     switch(props.node.type) {
@@ -18,6 +22,14 @@ const NodeRenderer = (props: any) => {
             return <DocumentLoaderNodeComponent {...props}/>
         case 'textSplitter':
             return <TextSplitterNodeComponent {...props}/>
+        case 'retriever':
+            return <RetrieverNodeComponent {...props}/>
+        case 'retrievalQA':
+            return <RetrievalQAComponent {...props}/>
+        case 'embedding':
+            return <EmbeddingNodeComponent {...props}/>
+        case 'vectorStore':
+            return <VectorStoreNodeComponent {...props}/>
     }
 }
 
